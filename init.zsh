ba4c7c3b7f149501da0e885d1c806945f784ae5c
Eric Nielsen <eric@amalgamar.com.br>
2019-12-09 09:20:37 -0500

    First commit
    Closes zimfw/zimfw#281

---
+++ init.zsh
@@ -0,0 +1,32 @@
+#
+# Aliases
+#
+
+# Homebrew
+alias brewc='brew cleanup'
+alias brewC='brew cleanup -s'
+alias brewd='brew doctor'
+alias brewe='brew edit'
+alias brewi='brew info'
+alias brewI='brew install'
+alias brewl='brew list'
+alias brewL='brew leaves'
+alias brewo='brew outdated'
+alias brews='brew search'
+alias brewu='brew update'
+alias brewU='brew upgrade --ignore-pinned'
+alias brewx='brew uninstall'
+alias brewX='brew uninstall --force'
+
+# Homebrew Cask
+alias cask='brew cask'
+alias caskd='cask doctor'
+alias caske='cask edit'
+alias caski='cask info'
+alias caskI='cask install'
+alias caskl='cask list'
+alias casko='cask outdated'
+alias caskU='cask reinstall'
+alias caskx='cask uninstall'
+alias caskX='cask uninstall --force'
+alias caskz='cask zap'
